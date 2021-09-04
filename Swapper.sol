c89970713e43d38d4c356c72c2eec9a67fa9c11a
CodeStorm <31198108+PrajwalMore@users.noreply.github.com>
2021-09-04 20:42:46 +0530

    Update Swapper.sol

--- Swapper.sol
+++ Swapper.sol
@@ -2,25 +2,20 @@ pragma solidity 0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
+
 contract Swapper is Ownable {
     // This will collect collection address.
-    mapping( string => address) public addressCollector;
+    mapping(string => address) public addressCollector;
 
-    struct NFT {
-        uint256 tokenId;
-        uint256 expectedPrice;
-        address to;
-        //ipfs uri
-    }
-    mapping(address => NFT) public addNFT;
+    //collection > tokenId > to
+    // collection {{tokenId : to},{tokenId : to},{tokenId : to}}
+    mapping(address => uint256) public collectionToTokenId;
 
-    struct DS {
-        uint256 tokenId;
-        address allowedTo;
-    }
-    mapping(uint256 => DS) public agreementMap;
+    mapping(address => mapping(uint256 => address)) public agreementMap;
 
-    ERC721 public nft;
+    ERC721 public nft1;
+    ERC721 public nft2;
 
     /* addCollection() will record all collection added for future swappings */
     function addCollections(string memory _collectionName, address _collection)
@@ -32,23 +27,23 @@ contract Swapper is Ownable {
         return true;
     }
 
-    function setAddress(address _collection1,address _collection2) external {
+    // it can be managed in UI to save the gas fees.
+    function setAddress(address _collection1, address _collection2) external {
         nft1 = ERC721(_collection1);
         nft2 = ERC721(_collection2);
     }
 
     //add bothaAgree records in addressCollector.
 
-    function bothAgree(
-        address owner1,
-        address owner2,
-        uint256 _tokenId1,
+    function isAgree(
+        address _owner,
+        uint256 _tokenId,
         address _collection
     ) public returns (bool) {
         //isAgree for swap.
-        address to = addNFT[_collection].to;
-        //require(to== msg.sender,"ERR_AUTORIZED_TO_YOU");
-        if (to != msg.sender) {
+        address to = agreementMap[_collection][_tokenId];
+        require(to == _owner, "ERR_AUTORIZED_TO_YOU");
+        if (to == _owner) {
             return true;
         } else {
             return false;
@@ -68,32 +63,36 @@ contract Swapper is Ownable {
         address _collection
     ) external returns (bool) {
         //transfer from caller to other accounts , if both are agree on deal.
-        // this contract make available method on collection address to NFT seller/buyer.
         require(
-            bothAgree(owner1, owner2, owner1Token, _collection),
+            isAgree(owner1, owner1Token, _collection),
             "BOTH_NEEDS_TO_AGREE"
         );
         require(
-            bothAgree(owner2, owner1, owner2Token, _collection),
+            isAgree(owner2, owner2Token, _collection),
             "BOTH_NEEDS_TO_AGREE"
         );
 
+        require(
+            msg.sender == owner1 || msg.sender == owner2,
+            "You are not authorized to swap other's tokens"
+        );
+        require(
+            msg.sender == owner2,
+            "You are not authorized to swap other's tokens"
+        );
         // Do with UI.
-        nft.safeTransferFrom(owner1, owner2, owner1Token);
-        nft.safeTransferFrom(owner2, owner1, owner2Token);
+        nft1.safeTransferFrom(owner1, owner2, owner1Token);
+        nft1.safeTransferFrom(owner2, owner1, owner2Token);
         return true;
     }
 
-    event evt(address ownerAddr);
+    event evt(address collection, uint256 TokenId, address to);
 
-function addAgreement(
+    function addAgreement(
         uint256 _tokenId,
         address _collection,
-        uint256 _expectedPrice,
         address _to
-    ) payable public returns(bool success){
-        //check for caller should be someone from user1 or user2.
-
+    ) public payable {
         // check if tokenId is token from that collection collection
         // string memory nm=nft.name();
         // require(keccak256(abi.encodePacked(nm))== keccak256(abi.encodePacked(addressCollector[_collection])), "ERR_COLLECTION_IS_DIFFERENT");
@@ -101,16 +100,11 @@ function addAgreement(
         // if caller is owner of `tokenId`.
         address ownerAddr = ERC721(_collection).ownerOf(_tokenId);
         require(ownerAddr == msg.sender, "NOT_TOKEN_OWNER");
-        addNFT[_collection].tokenId = _tokenId; // this would be updating previous state instead of adding one more record //need to change to array of mappings.
-        addNFT[_collection].expectedPrice = _expectedPrice;
-        addNFT[_collection].to = _to;
-        //ERC721(_collection).approve(address(this), _tokenId);
-        (bool success, ) = address(_collection).call(
-            abi.encodeWithSignature("approv(address,uint256)", address(this),_tokenId)
-        );
-        return success;
-        //emit evt(ownerAddr);
+        agreementMap[_collection][_tokenId] = _to; // this would be updating previous state instead of adding one more record //need to change to array of mappings.
+
+        emit evt(_collection, _tokenId, _to);
     }
 
 
+    //UI-> NFTContract >
 }
